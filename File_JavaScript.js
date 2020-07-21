19a751a1733d7ef55ae1104d7e2c64c744b53749
BaraniRam <68587411+BaraniRam@users.noreply.github.com>
2020-07-21 11:14:57 +0000

    First load

---
+++ File_JavaScript.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph.</p>
+
+<button type="button" onclick='document.getElementById("Demo").innerHTML = "Hello JavaScript"'>Click Me</button>
+</body>
+</html>
